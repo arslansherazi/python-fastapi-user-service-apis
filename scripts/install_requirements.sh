bc8e1db70b3834533ee167fcaeb48e5bd011fb76
Arslan <arslanhaider.dev@hotmail.com>
2023-07-02 15:48:35 +0500

    update requirements installation script

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 curl https://bootstrap.pypa.io/get-pip.py | python
 pip install pipenv
-pipenv --python 3.11
+pipenv --python 3.11  # create the virtual environment
+pipenv shell  # activate the virtual environment
 pipenv install
 pipenv install pre-commit
 pre-commit install
